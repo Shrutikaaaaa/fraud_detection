8ce0f7554368bc91901e71e9fbd69804b3b06c91
Shrutikaaaaa <shrutikagulave007@gmail.com>
2023-01-29 12:43:53 +0530

    added 3 new model to the code

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,12 @@
 
 print("adding all ml related code here")
+
+def model1():
+    print("Adding code to model1")
+
+
+def model2():
+    print("Adding code to model2")
+
+def model3():
+    print("Adding code to model3")
